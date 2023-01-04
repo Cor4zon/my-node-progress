81fd7c04def259c6c611b9f0521392f8e8343155
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-04 13:15:55 +0400

    Day 1

--- index-3.js
+++ index-3.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-
+    const method = req.method;
     res.setHeader('Content-Type', 'text/html');
+
     if (url === '/') {
-        res.write(fs.readFileSync('index-2.html'));
-    } else if (url === '/message') {
-        console.log(req);
         res.write(fs.readFileSync('index.html'));
+    } else if (url === 'message') {
+        res.write(fs.readFileSync('index-2.html'));
     }
     res.end();
 });
