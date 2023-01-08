a5b86e5720647c316fb29feefa808eaf27d91a3e
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-08 11:32:03 +0400

    Config project for day 6

---
+++ day6-pug/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const ShopRouter = require('./routes/shop');
+const AdminRouter = require('./routes/admin');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.use('/admin', AdminRouter);
+
+app.use('/shop', ShopRouter);
+
+app.use((err, res, req, next) => {
+    console.log(err);
+    res.status(404).send('<h1>Not Found</h1>');
+});
+
+app.listen(3000);
+
+
