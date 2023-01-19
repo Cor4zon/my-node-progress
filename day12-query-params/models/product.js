2def2c3b7b09ad59778652873695b9c01dda6f0b
Victor Fedorov <vpfedorov25@gmail.com>
2023-01-19 20:30:29 +0400

    day12 progress

---
+++ day12-query-params/models/product.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+const postCart = (req, res, next) => {
+  const prodId = req.body.prodId;
+  
+}
+
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.trunc(Math.random() * 1000).toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static fetchById(id, cb) {
+    getProductsFromFile(products => {
+      const productById = products.find(product => product.id == id);
+      console.log(productById);
+      cb(productById);
+    });
+  }
+};
