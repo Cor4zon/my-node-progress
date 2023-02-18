ed473f00ffb315f2149aa3f27bcb9becfe09d8d9
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-18 19:00:03 +0400

    day15

--- day15-sql/util/database.js
+++ day15-sql/util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+  host: 'localhost',
+  user: 'root',
+  database: 'node_quide',
+  password: 'adminqwerty',  
 });
 
 module.exports = pool.promise();
